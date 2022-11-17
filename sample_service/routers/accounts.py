804b202a36bcd49840ed968fa61af87e8dbde6af
James Hoffman <jamesvhoffman@outlook.com>
2022-11-17 17:48:59 -0500

    Added tags to routers

--- sample_service/routers/accounts.py
+++ sample_service/routers/accounts.py
@@ -32,14 +32,14 @@ class HttpError(BaseModel):
     detail: str
 
 
-router = APIRouter()
+router = APIRouter(tags=["Accounts"])
 
 
-@router.get("/api/protected", response_model=bool)
-async def get_protected(
-    account_data: dict = Depends(authenticator.get_current_account_data),
-):
-    return True
+# @router.get("/api/protected", response_model=bool)
+# async def get_protected(
+#     account_data: dict = Depends(authenticator.get_current_account_data),
+# ):
+#     return True
 
 
 @router.get("/token", response_model=AccountToken | None)
