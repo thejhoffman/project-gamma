a03a14489f58be09c590eadd8527ea6c1d276525
thvilla <106586180+thvilla@users.noreply.github.com>
2022-11-17 13:51:39 -0500

    updated journal

--- sample_service/migrations/000_define_domain.py
+++ sample_service/migrations/000_define_domain.py
@@ -1,7 +1,7 @@
 steps = [
     [
         # define citext domain
-        r"""
+        """
         CREATE EXTENSION citext;
         CREATE DOMAIN email AS citext
             CHECK ( value ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$' );
