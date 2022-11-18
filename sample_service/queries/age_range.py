b2ab9bd504e503940b328eebc1377127184af14c
James Hoffman <jamesvhoffman@outlook.com>
2022-11-18 14:37:21 -0500

    Added CRUD/error handling capability to age range

--- sample_service/queries/age_range.py
+++ sample_service/queries/age_range.py
@@ -1,6 +1,11 @@
 from pydantic import BaseModel
-from typing import List
+from typing import List, Union
 from queries.pool import pool
+
+
+class ErrorMessage(BaseModel):
+    message: str
+    code: int
 
 
 class AgeRangeIn(BaseModel):
@@ -13,7 +18,10 @@ class AgeRangeOut(BaseModel):
 
 
 class AgeRangeQueries:
-    def get_all(self) -> List[AgeRangeOut]:
+    # GET ALL AGE RANGES
+    def get_all(
+        self,
+    ) -> Union[List[AgeRangeOut], ErrorMessage]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -28,7 +36,122 @@ class AgeRangeQueries:
                         for record in result
                     ]
         except Exception:
-            return {"message": "Could get all age ranges"}
+            return ErrorMessage(
+                message="Could get all age ranges",
+                code=500,
+            )
+
+    # CREATE A NEW AGE RANGE
+    def create(
+        self,
+        age_range: AgeRangeIn,
+    ) -> Union[AgeRangeOut, ErrorMessage]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO age_range
+                            (age)
+                        VALUES
+                            (%s)
+                        RETURNING id;
+                        """,
+                        [age_range.age],
+                    )
+                    id = result.fetchone()[0]
+                    return self.age_range_in_to_out(id, age_range)
+        except Exception:
+            return ErrorMessage(
+                message="Create did not work",
+                code=500,
+            )
+
+    # GET DETAIL OF ONE AGE RANGE
+    def get_one(
+        self,
+        age_range_id: int,
+    ) -> Union[AgeRangeOut, ErrorMessage]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id, age
+                        FROM age_range
+                        WHERE id = %s
+                        """,
+                        [age_range_id],
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return ErrorMessage(
+                            message="Age range not found",
+                            code=404,
+                        )
+                    return self.record_to_age_range_out(record)
+        except Exception:
+            return ErrorMessage(
+                message="Could not get that age range",
+                code=500,
+            )
+
+    # UPDATE AN AGE RANGE
+    def update(
+        self,
+        age_range_id: int,
+        age_range: AgeRangeIn,
+    ) -> Union[AgeRangeOut, ErrorMessage]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        UPDATE age_range
+                        SET age = %s
+                        WHERE id = %s
+                        """,
+                        [age_range.age, age_range_id],
+                    )
+                    if result.rowcount == 0:
+                        return ErrorMessage(
+                            message="Age range not found",
+                            code=404,
+                        )
+                    return self.age_range_in_to_out(age_range_id, age_range)
+        except Exception:
+            return ErrorMessage(
+                message="Could not update that age range",
+                code=500,
+            )
+
+    # DELETE AN AGE RANGE
+    def delete(
+        self,
+        age_range_id: int,
+    ) -> Union[bool, ErrorMessage]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        DELETE FROM age_range
+                        WHERE id = %s
+                        """,
+                        [age_range_id],
+                    )
+                    if result.rowcount == 0:
+                        return ErrorMessage(
+                            message="Age range not found",
+                            code=404,
+                        )
+                    return True
+        except Exception:
+            return False
+
+    def age_range_in_to_out(self, id: int, age_range: AgeRangeIn):
+        old_data = age_range.dict()
+        return AgeRangeOut(id=id, **old_data)
 
     def record_to_age_range_out(self, record):
         return AgeRangeOut(id=record[0], age=record[1])
--- sample_service/routers/age_range.py
+++ sample_service/routers/age_range.py
@@ -1,10 +1,86 @@
-from fastapi import APIRouter, Depends
-from typing import List
-from queries.age_range import AgeRangeQueries, AgeRangeOut
+from fastapi import APIRouter, Depends, Response
+from typing import List, Union
+from queries.age_range import (
+    AgeRangeQueries,
+    AgeRangeIn,
+    AgeRangeOut,
+    ErrorMessage,
+)
 
 router = APIRouter(tags=["Age Range"])
 
 
-@router.get("/api/age_range", response_model=List[AgeRangeOut])
-def get_all(repo: AgeRangeQueries = Depends()):
+# GET ALL AGE RANGES
+@router.get(
+    "/api/age_range",
+    response_model=Union[List[AgeRangeOut], ErrorMessage],
+)
+def get_all(
+    repo: AgeRangeQueries = Depends(),
+) -> Union[List[AgeRangeOut], ErrorMessage]:
     return repo.get_all()
+
+
+# CREATE A NEW AGE RANGE
+@router.post(
+    "/api/age_range",
+    response_model=Union[AgeRangeOut, ErrorMessage],
+)
+def create_age_range(
+    age_range: AgeRangeIn,
+    repo: AgeRangeQueries = Depends(),
+) -> Union[AgeRangeOut, ErrorMessage]:
+    return repo.create(age_range)
+
+
+# GET DETAIL OF ONE AGE RANGE
+@router.get(
+    "/api/age_range/{age_range_id}",
+    response_model=Union[AgeRangeOut, ErrorMessage],
+)
+def get_one_age_range(
+    age_range_id: int,
+    response: Response,
+    repo: AgeRangeQueries = Depends(),
+) -> Union[AgeRangeOut, ErrorMessage]:
+    age_range = repo.get_one(age_range_id)
+    print(age_range)
+    response.status_code = set_response_code(age_range)
+    return age_range
+
+
+# UPDATE AN AGE RANGE
+@router.put(
+    "/api/age_range/{age_range_id}",
+    response_model=Union[AgeRangeOut, ErrorMessage],
+)
+def update_age_range(
+    age_range_id: int,
+    age_range: AgeRangeIn,
+    response: Response,
+    repo: AgeRangeQueries = Depends(),
+) -> Union[AgeRangeOut, ErrorMessage]:
+    age_range = repo.update(age_range_id, age_range)
+    response.status_code = set_response_code(age_range)
+    return age_range
+
+
+# DELETE AN AGE RANGE
+@router.delete(
+    "/api/age_range/{age_range_id}",
+    response_model=Union[bool, ErrorMessage],
+)
+def delete_age_range(
+    age_range_id: int,
+    response: Response,
+    repo: AgeRangeQueries = Depends(),
+) -> Union[bool, ErrorMessage]:
+    did_delete = repo.delete(age_range_id)
+    response.status_code = set_response_code(did_delete)
+    return did_delete
+
+
+# function to set response code
+def set_response_code(error_check):
+    if type(error_check) is ErrorMessage:
+        return error_check.code
