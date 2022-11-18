1fe205c563aec227bab2ca0c3604c616a574334c
kralk1100 <jonathan.roman.2020ba@outlook.com>
2022-11-18 16:28:21 -0500

    November 18 Journal Entry and edited Create Interest API Endpoint

--- sample_service/queries/interests.py
+++ sample_service/queries/interests.py
@@ -1,23 +1,31 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from queries.pool import pool
 
 
 class InterestsIn(BaseModel):
     interests: Optional[str]
 
+class InterestsOut(BaseModel):
+    id: int
+    interests: Optional[str]
+
 class InterestsRepository(BaseModel):
-    def create(interest: InterestsIn):
+    def create(self, interest: InterestsIn) -> InterestsOut:
         with pool.connection() as conn:
             with conn.cursor() as db:
                 result=db.execute(
                     """
-                   INSERT INTO events
-                    (interests)
+                    INSERT INTO interests
+                        (name)
                     VALUES
-                    (%s);
-                    """
+                        (%s)
+                    RETURNING id;
+                    """,
                 [
                     interest.interests
                 ]
                 )
+                id=result.fetchone()[0]
+                old_data=interest.dict()
+                return InterestsOut(id=id, **old_data)
