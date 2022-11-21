6cac2fc6f9a8a4bd2326c669076463a3afa50635
James Hoffman <jamesvhoffman@outlook.com>
2022-11-21 16:29:23 -0500

    Removed unneeded route comments

--- sample_service/routers/accounts.py
+++ sample_service/routers/accounts.py
@@ -35,13 +35,6 @@ class HttpError(BaseModel):
 router = APIRouter(tags=["Accounts"])
 
 
-# @router.get("/api/protected", response_model=bool)
-# async def get_protected(
-#     account_data: dict = Depends(authenticator.get_current_account_data),
-# ):
-#     return True
-
-
 @router.get("/token", response_model=AccountToken | None)
 async def get_token(
     request: Request,
