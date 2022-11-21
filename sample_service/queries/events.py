fc540bfa8906e725d3deccd2a3a5b0b18c7f968a
kralk1100 <jonathan.roman.2020ba@outlook.com>
2022-11-21 13:48:16 -0500

    Worked on Create Event API endpoint, Andrew worked on Get Event API Endpoint

--- sample_service/queries/events.py
+++ sample_service/queries/events.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
 from datetime import date
+from typing import List, Union
+from queries.pool import pool
+
+class Error(BaseModel):
+    message:str
 
 class EventIn(BaseModel):
     name: str
@@ -8,12 +13,63 @@ class EventIn(BaseModel):
     occasion_id: int
     account_id: int
 
+class EventOut(BaseModel):
+    id: int
+    name: str
+    date: date
+    person_id: int
+    occasion_id: int
+    account_id: int
 
-#  CREATE TABLE events (
-#             id SERIAL PRIMARY KEY NOT NULL,
-#             name VARCHAR(100) NOT NULL,
-#             date DATE NOT NULL,
-#             person_id INTEGER NOT NULL REFERENCES person("id") ON DELETE RESTRICT,
-#             occasion_id INTEGER REFERENCES occasion("id") ON DELETE RESTRICT,
-#             account_id SERIAL NOT NULL REFERENCES accounts("id") ON DELETE CASCADE
-#         );
+class EventRepository:
+    def create_event(self, event:EventIn) -> EventOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO events
+                            (name, date, person_id, occasion_id, account_id)
+                        VALUES
+                            (%s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            event.name,
+                            event.date,
+                            event.person_id,
+                            event.occasion_id,
+                            event.account_id
+                        ]
+                        )
+                    id=result.fetchone()[0]
+                    old_data=event.dict()
+                    return EventOut(id=id, **old_data)
+        except Exception:
+            return {"message": "Could not create event"}
+
+
+    def get_all(self): -> Union[Error, List[EventOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT name, date, person_id, occiasion_id, account_id
+                        FROM Events
+                        ORDER BY date;
+                        """
+                    )
+                    result = []
+                    for record in db:
+                        event = EventOut(
+                            name=record[0],
+                            date=record[1],
+                            person_id=record[2],
+                            occasion_id=record[3],
+                            account_id=record[4],
+                        )
+                        result.append(event)
+                    return result
+        except Exception:
+            return {"message": "Could not get all events"}
--- sample_service/routers/events.py
+++ sample_service/routers/events.py
@@ -5,7 +5,17 @@ from queries.events import EventIn, EventOut, EventRepository, Error
 router = APIRouter(tags=["Events"])
 
 
-@router.post("/api/events", tags=["events"])
+@router.post("/api/events", response_model=EventOut, tags=["events"])
 def create_event(event: EventIn):
-    print("event", event)
-    return event
+    event: EventIn
+    repo: EventRepository= Depends()
+    return repo.create(event)
+
+
+@router.get("/api/events", response_model= Union[Error, List[EventOut]])
+def get_all(
+    repo: EventOut = Depends(),
+):
+    return repo.get_all()
+
+@router.get("/api/events/")
