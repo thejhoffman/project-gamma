98b3d6d53c81f4e16573c796aa8b2ef0c2815afb
James Hoffman <jamesvhoffman@outlook.com>
2022-11-21 13:46:14 -0500

    Migration for initial data

---
+++ sample_service/migrations/002_set_initial_data.py
@@ -0,0 +1,73 @@
+steps = [
+    [
+        # data for age_range
+        """
+        INSERT INTO age_range
+        VALUES
+            (1, 'Children (under 14)'),
+            (2, 'Youth (15-24)'),
+            (3, 'Adults (25-64)'),
+            (4, 'Seniors (over 65)');
+        """,
+        # delete data from age_range
+        """
+        DELETE FROM age_range
+        """,
+    ],
+    [
+        # data for relationships
+        """
+        INSERT INTO relationships
+        VALUES
+            (1, 'Spouse'),
+            (2, 'Mother'),
+            (3, 'Father'),
+            (4, 'Colleague');
+        """,
+        # delete data from relationships
+        """
+        DELETE FROM relationships
+        """,
+    ],
+    [
+        # data for occasions
+        """
+        INSERT INTO occasion
+        VALUES
+            (1, 'Christmas', '2022-12-25'),
+            (2, 'Valentines', '2023-02-14'),
+            (3, 'Hanukkah', '2022-12-18');
+        """,
+        # delete data from occasions
+        """
+        DELETE FROM occasion
+        """,
+    ],
+    [
+        # data for interests
+        """
+        INSERT INTO interests
+        VALUES
+            (1, 'Movies'),
+            (2, 'Sports');
+        """,
+        # delete data from interests
+        """
+        DELETE FROM interests
+        """,
+    ],
+    [
+        # data for gender
+        """
+        INSERT INTO gender
+        VALUES
+            (1, 'Male'),
+            (2, 'Female'),
+            (3, 'Neutral');
+        """,
+        # delete data from occasions
+        """
+        DELETE FROM gender
+        """,
+    ],
+]
