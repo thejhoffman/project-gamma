bfd46a27437cfd46f359a0a4af09568018e4f7e5
thvilla <106586180+thvilla@users.noreply.github.com>
2022-11-22 16:18:28 -0500

    playing with database

--- sample_service/migrations/001_sample_migration.py
+++ sample_service/migrations/001_sample_migration.py
@@ -45,6 +45,18 @@ steps = [
         """,
     ],
     [
+        """
+        CREATE TABLE person (
+            id SERIAL PRIMARY KEY NOT NULL,
+            name VARCHAR(50) NOT NULL
+        );
+        """,
+        # "down" SQL statement for person table
+        """
+        DROP TABLE person;
+        """,
+    ],
+    [
         # creates interests table
         # "up" SQL statement
         """
@@ -88,19 +100,24 @@ steps = [
     ],
     [
         """
-        CREATE TABLE person (
-            id SERIAL PRIMARY KEY NOT NULL,
-            name VARCHAR(50) NOT NULL,
-            age_range_id INTEGER NOT NULL REFERENCES age_range("id") ON DELETE RESTRICT,
-            gender_id INTEGER REFERENCES gender("id") ON DELETE RESTRICT,
-            account_id SERIAL NOT NULL REFERENCES accounts("id") ON DELETE CASCADE,
-            interest_id INTEGER NOT NULL REFERENCES interests("id") ON DELETE RESTRICT,
-            relationship_id INTEGER NOT NULL REFERENCES relationships("id") ON DELETE RESTRICT
+        CREATE TABLE people (
+            person_id INTEGER NOT NULL,
+            interest_id INTEGER NOT NULL,
+            relationship_id INTEGER NOT NULL,
+            age_range_id INTEGER NOT NULL,
+            gender_id INTEGER NOT NULL,
+            account_id INTEGER NOT NULL,
+            CONSTRAINT people_pk PRIMARY KEY (interest_id, person_id),
+            CONSTRAINT fk_person FOREIGN KEY (person_id) REFERENCES person(id),
+            CONSTRAINT fk_interest FOREIGN KEY (interest_id) REFERENCES interests(id),
+            CONSTRAINT fk_relationship FOREIGN KEY (relationship_id) REFERENCES relationships(id),
+            CONSTRAINT fk_age_range FOREIGN KEY (age_range_id) REFERENCES age_range(id),
+            CONSTRAINT fk_gender FOREIGN KEY (gender_id) REFERENCES gender(id),
+            CONSTRAINT fk_account FOREIGN KEY (account_id) REFERENCES accounts(id)
         );
         """,
-        # "down" SQL statement for person table
         """
-        DROP TABLE person;
+        DROP TABLE people;
         """,
     ],
     [
