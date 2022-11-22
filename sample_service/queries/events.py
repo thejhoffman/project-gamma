a82fe58f976c93f2b31a6db6de6032a79bb32c12
James Hoffman <jamesvhoffman@outlook.com>
2022-11-22 16:03:04 -0500

    Cleaned up and added error handling

--- sample_service/queries/events.py
+++ sample_service/queries/events.py
@@ -1,11 +1,12 @@
 from pydantic import BaseModel
 from datetime import date
-from typing import List, Union, Optional
+from typing import List, Union
 from queries.pool import pool
 
 
-class Error(BaseModel):
+class ErrorMessage(BaseModel):
     message: str
+    code: int
 
 
 class EventIn(BaseModel):
@@ -13,7 +14,6 @@ class EventIn(BaseModel):
     date: date
     person_id: int
     occasion_id: int
-    # account_id: int
 
 
 class EventOut(BaseModel):
@@ -22,48 +22,46 @@ class EventOut(BaseModel):
     date: date
     person_id: int
     occasion_id: int
-    # account_id: int
 
 
 class EventRepository:
-    def update_event(
-        self, account_id: int, event_id: int, event: EventIn
-    ) -> Union[EventOut, Error]:
+    # GET ALL EVENTS
+    def get_all(
+        self,
+        account_id: int,
+    ) -> Union[List[EventOut], ErrorMessage]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    db.execute(
+                    result = db.execute(
                         """
-                        UPDATE events
-                        SET name = %s
-                         , date = %s
-                         , person_id = %s
-                         , occasion_id = %s
-                         , account_id = %s
-                         WHERE account_id = %s AND id = %s
-                         RETURNING id
-                                , name
-                                , person_id
-                                , occasion_id
-                                , account_id
+                        SELECT id
+                            , name
+                            , date
+                            , person_id
+                            , occasion_id
+                            , account_id
+                        FROM events
+                        WHERE account_id = %s
+                        ORDER BY date;
                         """,
-                        [
-                            event.name,
-                            event.date,
-                            event.person_id,
-                            event.occasion_id,
-                            account_id,
-                            account_id,
-                            event_id,
-                        ],
+                        [account_id],
                     )
-                    old_data = event.dict()
-                    return EventOut(id=event_id, **old_data)
-        except Exception as e:
-            print(e)
-            return {"message": "Could not update event"}
+                    return [
+                        self.record_to_event_out(record) for record in result
+                    ]
+        except Exception:
+            return ErrorMessage(
+                message="Could not get all events",
+                code=500,
+            )
 
-    def create_event(self, account_id: int, event: EventIn) -> EventOut:
+    #  CREATE A NEW PERSON
+    def create_event(
+        self,
+        account_id: int,
+        event: EventIn,
+    ) -> Union[EventOut, ErrorMessage]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -84,12 +82,19 @@ class EventRepository:
                         ],
                     )
                     id = db.fetchone()[0]
-                    old_data = event.dict()
-                    return EventOut(id=id, **old_data)
+                    return self.event_in_to_out(id, event)
         except Exception:
-            return {"message": "Could not create event"}
+            return ErrorMessage(
+                message="Could not create event",
+                code=500,
+            )
 
-    def get_all(self, account_id: int) -> Union[Error, List[EventOut]]:
+    # GET DETAIL OF ONE EVENT
+    def get_one(
+        self,
+        account_id,
+        event_id: int,
+    ) -> Union[EventOut, ErrorMessage]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -102,27 +107,76 @@ class EventRepository:
                             , occasion_id
                             , account_id
                         FROM events
-                        WHERE account_id = %s
-                        ORDER BY date;
+                        WHERE account_id = %s AND id = %s
                         """,
-                        [account_id],
+                        [account_id, event_id],
                     )
-                    result = []
-                    for record in db:
-                        event = EventOut(
-                            id=record[0],
-                            name=record[1],
-                            date=record[2],
-                            person_id=record[3],
-                            occasion_id=record[4],
-                            account_id=record[5],
+                    record = result.fetchone()
+                    if record is None:
+                        return ErrorMessage(
+                            message="Event not found",
+                            code=404,
                         )
-                        result.append(event)
-                    return result
+                    return self.record_to_event_out(record)
         except Exception:
-            return {"message": "Could not get all events"}
+            return ErrorMessage(
+                message="Could not get that event",
+                code=404,
+            )
 
-    def delete(self, account_id: int, event_id: int) -> bool:
+    # UPDATE AN EVENT
+    def update_event(
+        self,
+        account_id: int,
+        event_id: int,
+        event: EventIn,
+    ) -> Union[EventOut, ErrorMessage]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        UPDATE events
+                        SET name = %s
+                            , date = %s
+                            , person_id = %s
+                            , occasion_id = %s
+                            , account_id = %s
+                        WHERE account_id = %s AND id = %s
+                        RETURNING id
+                                , name
+                                , person_id
+                                , occasion_id
+                                , account_id
+                        """,
+                        [
+                            event.name,
+                            event.date,
+                            event.person_id,
+                            event.occasion_id,
+                            account_id,
+                            account_id,
+                            event_id,
+                        ],
+                    )
+                    if db.rowcount == 0:
+                        return ErrorMessage(
+                            message="Event not found",
+                            code=404,
+                        )
+                    return self.event_in_to_out(event_id, event)
+        except Exception:
+            return ErrorMessage(
+                message="Could not update event",
+                code=404,
+            )
+
+    # DELETE AN EVENT
+    def delete(
+        self,
+        account_id: int,
+        event_id: int,
+    ) -> Union[bool, ErrorMessage]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
@@ -133,36 +187,15 @@ class EventRepository:
                         """,
                         [account_id, event_id],
                     )
+                    if db.rowcount == 0:
+                        return ErrorMessage(
+                            message="Event not found", code=404
+                        )
                     return True
-        except Exception as e:
-            print(e)
+        except Exception:
             return False
 
-    def get_one(self, account_id, event_id: int) -> Optional[EventOut]:
-        try:
-            with pool.connection() as conn:
-                with conn.cursor() as db:
-                    result = db.execute(
-                        """
-                        SELECT id
-                            , name
-                            , date,
-                            , person_id
-                            , occasion_id
-                            , account_id
-                        FROM events
-                        WHERE account_id = %s AND id = %s
-                        """,
-                        [event_id]
-                    )
-                    record = result.fetchone()
-                    if record is None:
-                        return None
-                    return self.record_to_event_out(record)
-        except Exception as e:
-            print(e)
-            return {"message": "Could not get that event"}
-
+    # helper methods
     def event_in_to_out(self, id: int, event: EventIn):
         old_data = event.dict()
         return EventOut(id=id, **old_data)
