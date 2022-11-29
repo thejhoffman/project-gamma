670776792100636c406e61692521f9f6832e2490
thvilla <106586180+thvilla@users.noreply.github.com>
2022-11-29 15:25:38 -0500

    personform.js form post request working with token

--- sample_service/queries/products.py
+++ sample_service/queries/products.py
@@ -1,31 +1,7 @@
 import json
 import requests
-# import os
-
-# # ETSY_API_KEY = os.environ["ETSY_API_KEY"]
-
-# def get_product():
-#     params = {
-#         # "max_price": "100",
-#         # "includes": "images",
-#         # "taxonomy_id": "1432",
-#         # "keywords": f"{interest}",
-#         "api_key": "o2y8damjweefvmlsytbbz2fm"
-#     }
-#     url = "https://api.etsy.com/v3/application/openapi-ping"
-#     response = requests.get(url, params=params)
-#     print(response)
-#     content = json.loads(response.content)
-
-#     try:
-#         return {"product_info": content["count"]}
-#     except (KeyError,IndexError):
-#         return {"product_info": "cannot get product"}
-
-
 from pydantic import BaseModel
 import os
-from typing import Optional
 
 ETSY_API_KEY = os.environ["ETSY_API_KEY"]
 
