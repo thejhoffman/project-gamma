0511ee36921bc70edbf02faecd93efaf311767af
thvilla <106586180+thvilla@users.noreply.github.com>
2022-11-28 21:34:37 -0500

    etsy api get request working

--- sample_service/main.py
+++ sample_service/main.py
@@ -10,19 +10,21 @@ from routers import (
     occasions,
     gender,
     people,
+    products
 )
 import os
 
 app = FastAPI()
 app.include_router(authenticator.router)
 app.include_router(accounts.router)
-app.include_router(relationships.router)
-app.include_router(occasions.router)
-app.include_router(events.router)
-app.include_router(age_range.router)
-app.include_router(interests.router)
-app.include_router(gender.router)
+# app.include_router(relationships.router)
+# app.include_router(occasions.router)
+# app.include_router(events.router)
+# app.include_router(age_range.router)
+# app.include_router(interests.router)
+# app.include_router(gender.router)
 app.include_router(people.router)
+app.include_router(products.router)
 
 app.add_middleware(
     CORSMiddleware,
