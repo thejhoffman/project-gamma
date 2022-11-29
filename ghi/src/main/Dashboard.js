a7d041dd86a40d8393451ca1eab403aaad8b5cc1
James Hoffman <jamesvhoffman@outlook.com>
2022-11-29 17:37:38 -0500

    Updates to dashboard, still WIP

--- ghi/src/main/Dashboard.js
+++ ghi/src/main/Dashboard.js
@@ -2,13 +2,19 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 
 const Dashboard = () => {
-  const [formData, setFormData] = useState({
-    person_id: ""
-  });
+  const [person_id, setPerson] = useState();
   const [people, setPeople] = useState([]);
 
-  const handleFormData = async (e) => {
-    setFormData(prev => ({ ...prev, [e.target.name]: e.target.value }));
+  const handlePersonData = async (e) => {
+    setPerson(prev => (e.target.value));
+  };
+
+  const handleEditPerson = async (e) => {
+    e.preventDefault();
+  };
+
+  const handleDeletePerson = async (e) => {
+    e.preventDefault();
   };
 
   useEffect(() => {
@@ -25,37 +31,56 @@ const Dashboard = () => {
     fetchData();
   }, []);
 
+
   return (
-    <div className=" container mt-2">
+    <div className="container mt-2 shadow p-4 mt-4">
       <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
-            <h1>Select a person</h1>
-            <form>
-              <div className="mb-3">
-                <select
-                  onChange={handleFormData}
-                  value={formData.person_id}
-                  className="form-select"
-                  required
-                  id="person_id"
-                  name="person_id"
-                >
-                  <option value="">Choose a person</option>
-                  {people.map(person => {
-                    return (
-                      <option key={person.id} value={person.id}>
-                        {`${person.name}`}
-                      </option>
-                    );
-                  })}
-                </select>
-              </div>
-              <button className="btn btn-primary">Add</button>
-            </form>
-          </div>
+        <div className="col">
+          <select
+            onChange={handlePersonData}
+            value={person_id}
+            className="form-select"
+            required
+            id="person_id"
+            name="person_id"
+          >
+            <option value="0">Choose a person</option>
+            {people.map(person => {
+              return (
+                <option key={person.id} value={person.id}>
+                  {`${person.name}`}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="col-auto">
+          <button
+            className="btn btn-primary"
+            onClick={handleEditPerson}
+            style={{ marginRight: '1em' }}
+          >
+            Edit
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={handleDeletePerson}
+          >
+            Delete
+          </button>
         </div>
       </div>
+
+      <hr />
+
+      <div className="row">
+        <h3>Upcoming events</h3>
+      </div>
+
+      <hr />
+
+
     </div>
   );
 };
