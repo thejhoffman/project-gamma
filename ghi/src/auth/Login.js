7ad2b551fdc18b76389c4ae425e02b5c8f9da42b
James Hoffman <jamesvhoffman@outlook.com>
2022-12-03 22:56:25 -0500

    Changed bootstrap classes & added a password check

--- ghi/src/auth/Login.js
+++ ghi/src/auth/Login.js
@@ -7,7 +7,7 @@ function BootstrapInput(props) {
     const { id, placeholder, labelText, value, onChange, type } = props;
 
     return (
-        <div className="offset-3 col-6">
+        <div className="mb-3">
             <label htmlFor={id} className="form-label">{labelText}</label>
             <input value={value} onChange={onChange} required type={type} className="form-control" id={id} placeholder={placeholder} />
         </div>
@@ -39,28 +39,33 @@ function Login(props) {
     }, [navigate, result.isSuccess]);
 
     return (
-
-        <form onSubmit={handleLogin}>
-            <BootstrapInput
-                id="email"
-                placeholder="you@example.com"
-                labelText="Email Login"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                type="email" />
-            <BootstrapInput
-                id="password"
-                placeholder="Password"
-                labelText="Password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                type="password" />
-            <div className="form-floating mb-3">
-                <button
-                    disabled={password.length === 0}
-                    className="btn btn-primary offset-3 col-6">Submit</button>
+        <div className="container mt-2">
+            <div className='offset-3 col-6'>
+                <div className="shadow p-4 mt-4">
+                    <form onSubmit={handleLogin}>
+                        <BootstrapInput
+                            id="email"
+                            placeholder="you@example.com"
+                            labelText="Email Login"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                            type="email" />
+                        <BootstrapInput
+                            id="password"
+                            placeholder="Password"
+                            labelText="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                            type="password" />
+                        <div className="form-floating mb-3">
+                            <button
+                                disabled={password.length === 0}
+                                className="btn btn-primary offset-3 col-6">Submit</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     );
 }
 
