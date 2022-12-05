8320950a875be93888dc25c75e050d1b4378329a
James Hoffman <jamesvhoffman@outlook.com>
2022-12-04 21:33:59 -0500

    Completed unit tests for age range

--- sample_service/tests/test_age_range_routes.py
+++ sample_service/tests/test_age_range_routes.py
@@ -12,10 +12,37 @@ class AgeRangeQueriesMock:
 
     def create(self, age_range):
         response = {
-            "id": 1,
-            "age": "Test Range",
+            "id": 0,
+            "age": "string",
         }
+        response.update({"id": 1})
         response.update(age_range)
+        return response
+
+    def get_one(self, id):
+        response = {
+            "id": 1,
+            "age": "Adults (25-64)",
+        }
+        if response.get("id") == id:
+            return response
+
+    def update(self, id, age_range):
+        response = {
+            "id": 1,
+            "age": "string",
+        }
+        if response.get("id") == id:
+            response.update(age_range)
+            return response
+
+    def delete(self, id):
+        response = {
+            "id": 1,
+            "age": "string",
+        }
+        if response.get("id") == id:
+            response = True
         return response
 
 
@@ -28,7 +55,7 @@ def test_get_all_age_ranges():
 
     # Assert
     assert response.status_code == 200
-    # assert response.json() == {"trucks": []}
+    assert response.json() == []
 
     # Cleanup
     app.dependency_overrides = {}
@@ -38,7 +65,7 @@ def test_create_age_range():
     # Arrange
     app.dependency_overrides[AgeRangeQueries] = AgeRangeQueriesMock
     age_range = {
-        "age": "Test Range",
+        "age": "Adults (25-64)",
     }
 
     # Act
@@ -49,7 +76,68 @@ def test_create_age_range():
 
     # Assert
     assert response.status_code == 200
-    assert response.json()["age"] == "Test Range"
+    assert response.json()["id"] == 1
+    assert response.json()["age"] == "Adults (25-64)"
+
+    # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_get_one_age_range():
+    # Arrange
+    app.dependency_overrides[AgeRangeQueries] = AgeRangeQueriesMock
+    id = 1
+
+    # Act
+    response = client.get(
+        f"/api/age_range/{id}",
+    )
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["age"] == "Adults (25-64)"
+
+    # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_update_age_range():
+    # Arrange
+    app.dependency_overrides[AgeRangeQueries] = AgeRangeQueriesMock
+    id = 1
+    age_range = {
+        "age": "Adults (25-64)",
+    }
+
+    # Act
+    response = client.put(
+        f"/api/age_range/{id}",
+        json.dumps(age_range),
+    )
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["age"] == "Adults (25-64)"
+
+    # Cleanup
+    app.dependency_overrides = {}
+
+
+def test_delete_age_range():
+    # Arrange
+    app.dependency_overrides[AgeRangeQueries] = AgeRangeQueriesMock
+    id = 1
+
+    # Act
+    response = client.delete(
+        f"/api/age_range/{id}",
+    )
+
+    # Assert
+    assert response.status_code == 200
     assert response.json()
 
     # Cleanup
+    app.dependency_overrides = {}
