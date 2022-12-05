64d43613d9007103e263b789a7917eddb71e2ca7
James Hoffman <jamesvhoffman@outlook.com>
2022-12-05 16:53:52 -0500

    Minor tweak to auth check

--- ghi/src/utils/PrivateRoutes.js
+++ ghi/src/utils/PrivateRoutes.js
@@ -2,11 +2,12 @@ import { Outlet, Navigate } from "react-router-dom";
 import { useGetTokenQuery } from "../store/tokenApi";
 
 const PrivateRoutes = () => {
-  const { data: tokenData, isSuccess } = useGetTokenQuery();
+  const { data: tokenData, isSuccess, isFetching } = useGetTokenQuery();
   let auth = tokenData !== null && isSuccess;
-  return (
-    auth ? <Outlet /> : <Navigate to="/login" />
-  );
+  if (!isFetching)
+    return (
+      auth ? <Outlet /> : <Navigate to="/login" />
+    );
 };
 
 export default PrivateRoutes;
