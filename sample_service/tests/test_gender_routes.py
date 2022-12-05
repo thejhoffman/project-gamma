afa0da97b3c43e9bb6bf95dddcaa0310b6959c94
Andrew Collins <andrewxcollins@gmail.com>
2022-12-05 15:02:35 -0700

    added unit test for gender

---
+++ sample_service/tests/test_gender_routes.py
@@ -0,0 +1,49 @@
+from fastapi.testclient import TestClient
+from main import app
+from routers.gender import GenderRepository
+import json
+
+client = TestClient(app)
+
+
+class GenderRepositoryMock:
+    def get_all(self):
+        return []
+
+    def create(self, gender):
+        response = {
+            "id": 0,
+            "name": "string",
+        }
+        response.update({"id": 1})
+        response.update(gender)
+        return response
+
+
+def test_create_gender():
+    app.dependency_overrides[GenderRepository] = GenderRepositoryMock
+    gender = {
+        "name": "Gender Male",
+    }
+
+    response = client.post(
+        "/api/gender",
+        json.dumps(gender),
+    )
+
+    assert response.status_code == 200
+    assert response.json()["id"] == 1
+    assert response.json()["name"] == "Gender male"
+
+    app.dependency_overrides = {}
+
+
+def test_get_all_gender():
+    app.dependency_overrides[GenderRepository] = GenderRepository
+
+    response = client.get("/api/gender")
+
+    assert response.status_code == 200
+    assert response.json() == []
+
+    app.dependency_overrides = {}
